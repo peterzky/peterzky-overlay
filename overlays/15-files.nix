19a4b2636f79f5631aaad8ff43e5396ffef6b11e
peterzky <378096232@qq.com>
2020-11-17 04:46:53 +0800

    add chnroute and dnscrypt files

---
+++ overlays/15-files.nix
@@ -0,0 +1,7 @@
+self:
+super {
+  chnroute =
+    super.writeText "chnrout.ipset" builtins.readFile ../files/chnroute.ipset;
+  dnscrypt-proxy = super.writeText "dnscrypt-proxy.toml" builtins.readFile
+    ../files/dnscrypt-proxy.toml;
+}
