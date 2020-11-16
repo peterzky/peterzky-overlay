4eb16f25e7a0132ac74b58059fc3378c93784e91
peterzky <378096232@qq.com>
2020-11-17 04:56:20 +0800

    fix format

--- overlays/15-files.nix
+++ overlays/15-files.nix
@@ -1,6 +1,6 @@
 self: super: {
   chnroute =
-    super.writeText "chnrout.ipset" builtins.readFile ../files/chnroute.ipset;
-  dnscrypt-proxy = super.writeText "dnscrypt-proxy.toml" builtins.readFile
-    ../files/dnscrypt-proxy.toml;
+    super.writeText "chnrout.ipset" (builtins.readFile ../files/chnroute.ipset);
+  dnscrypt-proxy = super.writeText "dnscrypt-proxy.toml"
+    (builtins.readFile ../files/dnscrypt-proxy.toml);
 }
