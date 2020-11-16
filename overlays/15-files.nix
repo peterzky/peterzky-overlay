441b6dd48285eba1a9b5af704be0e38a65597204
peterzky <378096232@qq.com>
2020-11-17 05:11:09 +0800

    rename chnroute -> chnroute-ipset, dnscrypt-proxy -> dnscrypt-proxy-conf

--- overlays/15-files.nix
+++ overlays/15-files.nix
@@ -1,6 +1,6 @@
 self: super: {
-  chnroute =
+  chnroute-ipset =
     super.writeText "chnrout.ipset" (builtins.readFile ../files/chnroute.ipset);
-  dnscrypt-proxy = super.writeText "dnscrypt-proxy.toml"
+  dnscrypt-proxy-conf = super.writeText "dnscrypt-proxy.toml"
     (builtins.readFile ../files/dnscrypt-proxy.toml);
 }
