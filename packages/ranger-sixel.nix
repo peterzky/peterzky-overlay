fc70b4ee789345169fe7a494e405598a506b5282
peterzky <378096232@qq.com>
2020-11-19 09:16:03 +0800

    ranger update

--- packages/ranger-sixel.nix
+++ packages/ranger-sixel.nix
@@ -1,5 +1,5 @@
 { stdenv, lib, fetchFromGitHub, python3Packages, file, less, highlight
-, imagePreviewSupport ? true, w3m ? null}:
+, imagePreviewSupport ? true, w3m ? null }:
 
 with stdenv.lib;
 
@@ -12,8 +12,9 @@ python3Packages.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "3ap";
     repo = "ranger";
-    rev = "70a0352d6247a388994051da0114aa998e93ce81";
-    sha256= "1irval1x4cqhw4s14k3yl0fsccxyz1dghsh9cd614d38abswwl6n";
+    rev = "240d8a0ea1b82690372ba067c24a3f581a29c782";
+    # date = 2020-08-01T12:19:50+02:00;
+    sha256 = "1pd9mw2yac2ndcngh41nkg0p7x9kly9q8ia6mymagvpwafjfcimn";
   };
 
   LC_ALL = "en_US.UTF-8";
@@ -22,9 +23,7 @@ python3Packages.buildPythonApplication rec {
   propagatedBuildInputs = [ file ]
     ++ lib.optionals (imagePreviewSupport) [ python3Packages.pillow ];
 
-  checkPhase = ''
-
-  '';
+  checkPhase = "\n";
 
   preConfigure = ''
     ${lib.optionalString (highlight != null) ''
@@ -36,7 +35,9 @@ python3Packages.buildPythonApplication rec {
       --replace "/bin/echo" "echo"
 
     substituteInPlace ranger/__init__.py \
-      --replace "DEFAULT_PAGER = 'less'" "DEFAULT_PAGER = '${stdenv.lib.getBin less}/bin/less'"
+      --replace "DEFAULT_PAGER = 'less'" "DEFAULT_PAGER = '${
+        stdenv.lib.getBin less
+      }/bin/less'"
 
     for i in ranger/config/rc.conf doc/config/rc.conf ; do
       substituteInPlace $i --replace /usr/share $out/share
@@ -54,7 +55,7 @@ python3Packages.buildPythonApplication rec {
       --replace "set preview_images false" "set preview_images true"
   '';
 
-  meta =  with lib; {
+  meta = with lib; {
     description = "File manager with minimalistic curses interface";
     homepage = "http://ranger.github.io/";
     license = licenses.gpl3;
