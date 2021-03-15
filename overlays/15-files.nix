4111a7740fbb2af34a43a85f8f0a3776bc5cfe6d
peterzky <378096232@qq.com>
2021-03-16 02:39:48 +0800

    gfw-white-list: update script

--- overlays/15-files.nix
+++ overlays/15-files.nix
@@ -1,6 +1,4 @@
 self: super: {
   chnroute-ipset =
     super.writeText "chnrout.ipset" (builtins.readFile ../files/chnroute.ipset);
-  dnscrypt-proxy-conf = super.writeText "dnscrypt-proxy.toml"
-    (builtins.readFile ../files/dnscrypt-proxy.toml);
 }
--- packages/gfw-white-list/default.nix
+++ packages/gfw-white-list/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation rec {
   name = "gfw-white-list";
   src = fetchgit {
-    url = "https://github.com/felixonmars/dnsmasq-china-list.git";
+    url = "http://github.com/felixonmars/dnsmasq-china-list.git";
     rev = "5637f3d7c3ed6622ca14b55cdea3305a27c41152";
     sha256 = "12jp412hm6ww60qbzbmyjl8sk9wmnyqmyv9lnhhrvpsb6zyrhmkb";
     # date = 2020-11-02T10:17:12+00:00;
