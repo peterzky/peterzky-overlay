475e7deba08f3d13300a5681128720fdd8b19e6b
peterzky <378096232@qq.com>
2021-04-22 22:16:49 +0800

    gfw updates

--- packages/gfw-white-list/default.nix
+++ packages/gfw-white-list/default.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation rec {
   name = "gfw-white-list";
   src = fetchgit {
     url = "http://github.com/felixonmars/dnsmasq-china-list.git";
-    rev = "540a3e59bc9201f8fb9296a21552ab429dc56e39";
-    sha256 = "06mzz4q15bms9w57sxh16c162d25k31i4alrmsk381gdy5z8rcab";
+    rev = "ea3d0dcfffa3bc326b2bb1002cbcb40a93ed38e3";
+    sha256 = "1kynw6wn1vmkjn9lfa0x2f81sxdrjdq54qmgaqjrv52ciydz05vk";
     # date = 2020-11-02T10:17:12+00:00;
   };
   buildPhase = ''
