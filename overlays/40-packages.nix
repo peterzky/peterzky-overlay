8d8669cd6046255cdfde5a820c6e5b572c889618
peterzky <378096232@qq.com>
2021-06-18 20:37:01 +0800

    wayst: -> 2021-06-16

--- overlays/40-packages.nix
+++ overlays/40-packages.nix
@@ -12,12 +12,13 @@ final: prev:
   # packages
   wayst-git = prev.wayst.overrideAttrs (
     oldAttrs: rec {
+      version = "2021-06-16";
       src = prev.fetchFromGitHub {
         owner = "91861";
         repo = "wayst";
-        rev = "e72ca78ef72c7b1e92473a98d435a3c85d7eab98";
-        # date = 2021-04-05T19:50:37+02:00;
-        sha256 = "112jajl34xl4i82bh9zal47jcqjmml6k6a0f985gz4v9ym4iaw2i";
+        rev = "b3722bb60828d443c07667b01031af68769db9df";
+        # date = 2021-06-16T19:42:38+02:00;
+        sha256 = "0jv5jmikivsm7ark9sxf16dhm61vy17vqip0w3c9dcbn6ysbhjml";
       };
     }
   );
