c6292595f360748f5beed416e994352dd9f74392
peterzky <378096232@qq.com>
2022-02-27 23:45:43 +0800

    auto update

--- overlays/15-python.nix
+++ overlays/15-python.nix
@@ -1,29 +1,26 @@
 final: prev:
 let
   myOverrides = python-final: python-prev:
-    let
-      _callPackage = prev.lib.callPackageWith (python-final // {
-        inherit (prev) fetchFromGitHub lib;
-      });
-    in
+    with prev.python3.pkgs;
     rec {
-      fuo = _callPackage ../pkgs/python/feeluown/base.nix {
+      fuo = callPackage ../pkgs/python/feeluown/base.nix {
         mpv = prev.mpv;
         wrapQtAppsHook = prev.qt5.wrapQtAppsHook;
         extensions = [ fuo-netease fuo-local fuo-qqmusic ];
       };
 
-      fuo-netease = _callPackage ../pkgs/python/feeluown/netease.nix { };
+      fuo-netease = callPackage ../pkgs/python/feeluown/netease.nix { };
 
-      fuo-local = _callPackage ../pkgs/python/feeluown/local.nix { };
+      fuo-local = callPackage ../pkgs/python/feeluown/local.nix { };
 
-      fuo-qqmusic = _callPackage ../pkgs/python/feeluown/qqmusic.nix { };
+      fuo-qqmusic = callPackage ../pkgs/python/feeluown/qqmusic.nix { };
 
     };
 
 in
 rec {
   python3 = prev.python3.override {
+    self = python3;
     packageOverrides = prev.lib.composeManyExtensions
       [ myOverrides ];
   };
